9ba76e5b3dea839d5ddb081c13298828a6e382dd
ledidi04 <enilsonetienne04@gmail.com>
2026-05-14 12:34:54 -0700

    modified:   ns4/index.php

--- ns4/index.php
+++ ns4/index.php
@@ -344,6 +344,13 @@ $siteUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : '
                     <span class="type-name">Formules & Fiches</span>
                     <span class="type-desc">Maths, Physique, Chimie, SVT…</span>
                 </a>
+
+                <!-- Programmes Officiels -->
+                <a href="<?= $basePath ?>/ns4/programmes.php" class="type-card card-programmes" title="Programmes officiels MENFP 9ème AF">
+                    <div class="type-icon-wrapper">📚</div>
+                    <span class="type-name">Programmes Officiels</span>
+                    <span class="type-desc">Chimie, Maths, SVT, Sciences...</span>
+                </a>
             </div>
 
            
